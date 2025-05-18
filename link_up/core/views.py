9e4473075bcf8a129378fb721c0a86e0c800e95a
kucseramilan <kucseramilna@gmail.com>
2025-05-18 20:48:28 +0200

    Uzenet irasa csoportba

--- link_up/core/views.py
+++ link_up/core/views.py
@@ -14,7 +14,7 @@ from core.forms import CustomLoginForm
 from django.db import connection
 from django.http import HttpResponse
 
-from .models import Felhasznalo, Bejegyzes, FelhasznaloKapcsolat, Csoport, FelhasznaloCsoport
+from .models import Felhasznalo, Bejegyzes, FelhasznaloKapcsolat, Csoport, FelhasznaloCsoport, Uzenet
 from .service import bejegyzes_service, image_service
 from core.enums import BejegyzesResponse, ImageCreationResponse, KapcsolatStatus, from_string, UzenetResponse
 from .service.bejegyzes_service import add_komment
@@ -301,6 +301,8 @@ def csoportok_view(request):
         Q(Q(jelolo=request.user) | Q(jelolt=request.user)) & Q(statusz=KapcsolatStatus.ACCEPTED.name.lower()))
     ismeros_id_list = []
     sajat_csoportok_id_list = []
+    active_group = None
+    group_name = None
 
     if request.method == 'POST':
         if "addGroup" in request.POST:
@@ -327,9 +329,25 @@ def csoportok_view(request):
                 )
 
             return redirect('csoportok')
+
         elif "removeGroup" in request.POST:
             torlendo_csoport_id = request.POST.get("csoport-id")
             Csoport.objects.filter(pk=torlendo_csoport_id).delete()
+
+        elif "guzenet" in request.POST:
+            gtext = request.POST.get("gtext")
+            kuldo_id = request.POST.get("kuldo-id")
+            csoport_id = request.POST.get("csoport-id")
+            csoport = Csoport.objects.get(pk=csoport_id)
+            active_group = csoport.id
+            group_name = csoport.csoport_nev
+
+            Uzenet.objects.create(
+                felhasznalo=Felhasznalo.objects.get(pk=kuldo_id),
+                csoport=csoport,
+                kuldesi_ido=datetime.datetime.now(),
+                tartalom=gtext,
+            )
 
     for kapcsolat in ismerosok:
         if kapcsolat.jelolo.id != request.user.id:
@@ -353,4 +371,6 @@ def csoportok_view(request):
         'felhasznalo_name': felhasznalo_name,
         'ismerosok': ismerosok,
         'csoportok': csoportok,
+        'active_chat': active_group,
+        'group_name': group_name,
     })
